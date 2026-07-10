3c91844ecf1912e55cabd52dc079640661f9ff29
SantiagoNavarrete <santinavarrete54gmail.com>
2026-07-10 19:15:36 -0300

    Boton nav

--- Navaweb/src/components/Navigation.jsx
+++ Navaweb/src/components/Navigation.jsx
@@ -5,7 +5,7 @@ export default function Navigation() {
   return (
     <nav className="nav">
       <div className="nav-logo">
-        <a href="hero"><Logo size="32px" variant="icon" /></a>
+        <li><a href="hero"><Logo size="32px" variant="icon" /></a></li>
       </div>
       <ul className="nav-links">
         <li><a href="#servicios">Servicios</a></li>
